bded50520bf57672003674765c5137789332fa19
Margarita <margo.himera@yandex.ru>
2018-08-19 12:37:21 +0300

    Script for searching strings was added

---
+++ find.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script for simple searching strings in Android project.
+# Move this file into AndroidStudioProjects folder.
+
+USAGE="Usage: find <project_name> <searched_string> <options> [--help]"
+INFO="Find string in Android project."
+
+if [[ $# < 2 || $1 == '--help' ]]; then
+	echo $USAGE
+	echo $INFO
+else
+	grep --color -rnw $1 -e $2 $3
+fi
